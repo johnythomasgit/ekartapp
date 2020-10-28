f4ee63e27610933d57eebc632eda1b654e1ca177
johnythomasgit <johnythomas.in@gmail.com>
2020-10-28 19:41:22 +0530

    Swagger api doc tool added

--- src/main/java/com/underscore/ekartapp/config/WebSecurityConfiguration.java
+++ src/main/java/com/underscore/ekartapp/config/WebSecurityConfiguration.java
@@ -41,6 +41,18 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
     public WebSecurityConfiguration() {
         super(true);
@@ -62,7 +74,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .securityContext().and()
                 .anonymous().and()
                 .authorizeRequests()
-                .antMatchers(GET, "/").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers(POST, "/users").anonymous()
                 .antMatchers(GET, "/media/downloadFile/**").anonymous()
                 .antMatchers("/login/**").anonymous()
--- src/main/java/com/underscore/ekartapp/controller/HealthCheckController.java
+++ src/main/java/com/underscore/ekartapp/controller/HealthCheckController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HealthCheckController {
-    @GetMapping("/")
+    @GetMapping("/healthcheck")
     public ResponseView healthCheck() {
         return new ResponseView("OK");
     }
