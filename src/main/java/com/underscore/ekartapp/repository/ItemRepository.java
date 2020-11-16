666055ab86e94c8880e337d79f57573d64271faf
johnythomasgit <johnythomas.in@gmail.com>
2020-11-16 21:11:32 +0530

    changes for native query filter

--- src/main/java/com/underscore/ekartapp/repository/ItemRepository.java
+++ src/main/java/com/underscore/ekartapp/repository/ItemRepository.java
@@ -7,6 +7,7 @@ package com.underscore.ekartapp.repository;
 
 import com.underscore.ekartapp.entity.Item;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 /**
@@ -16,7 +17,10 @@ import org.springframework.data.repository.Repository;
 public interface ItemRepository extends Repository<Item, Integer> {
     Item findById(Integer itemId);
     List<Item> findAll();
+    
     List<Item> findByStatusOrderByUpdatedDateDesc(short status);
+    @Query(value="SELECT i.* from item i WHERE i.stats=?1 AND i.name like %?2% AND IF(?3>0,i.categoryId=?3,true) AND IF(?4, a.upddatedDate > now()- INTERVAL 1 DAY, true)",nativeQuery = true)
+    List<Item> getItemList(short status,String key,Integer categoryId,Boolean isFresh);
     Item save(Item item);
     void deleteById(Integer id);
     
